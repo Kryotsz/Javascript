ed9030f4e16a2fb882c57505f0f5ed8fd3ca32e4
Igor Gassen <igor.gassen@awright.com.br>
2024-03-26 14:39:40 -0300

    Operadores lógicos

---
+++ CFBCursos/aula007/aula007.js
@@ -0,0 +1,13 @@
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1, n2, n3, n4
+n1 = 10
+n2 = 5
+n3 = 15
+n4 = 2
+
+console.log(n1 + " é maior que " + n2 + ": " + (n1>n2))
+console.log(n1 + " é maior que " + n3 + ": " + (n1>n3))
+console.log("Ambas as afirmações são verdadeiras: " + ((n1>n2)&&(n1>n3)))
