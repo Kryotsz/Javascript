08637920cd392996e58b85bb9176a0b3bdb7f028
Igor Gassen <igor.gassen@awright.com.br>
2024-03-27 11:44:22 -0300

    Diferença entre Pré e Pós Incremento

---
+++ CFBCursos/aula009/aula009.js
@@ -0,0 +1,18 @@
+let n = 10
+
+n += 10
+n -= 10
+n *= 2
+n /= 2
+
+// pós-incremento, vai utilizar o valor atual e só depois incrementar
+console.log(n++)
+
+// pré-incremento, vai incrementar o valor para depois usá-lo
+console.log(++n)
+
+let n2 = 10
+// operador de inversão (negação)
+let x =- n2
+
+console.log(x)
